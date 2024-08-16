696c05043da9f92371d54acafcc2e700c6e09166
DevGlauber <devglaubermonteiro@gmail.com>
2024-08-16 17:48:28 -0300

    adicionado add workpaces no sidebar

--- app/(plataform)/(dashboard)/organization/layout.tsx
+++ app/(plataform)/(dashboard)/organization/layout.tsx
@@ -1,3 +1,5 @@
+import Sidebar from '../_components/sidebar'
+
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl">
